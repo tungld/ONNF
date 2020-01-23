34d182fc3ac947f02f177e1a94d6bbca049b473b
Tung D. Le <tung@jp.ibm.com>
2020-01-23 16:31:21 +0900

    Infer shape for Unsqueeze

--- src/dialect/onnx/onnx_ops.cpp
+++ src/dialect/onnx/onnx_ops.cpp
@@ -624,6 +624,44 @@ LogicalResult verify(ONNXConvNoBiasOp op) {
 }
 
 //===----------------------------------------------------------------------===//
+// Unsqueeze
+
+void ONNXUnsqueezeOp::inferShapes() {
+  auto operandTy = getOperand().getType().cast<RankedTensorType>();
+  int inRank = operandTy.getRank();
+
+  auto axisAttrs = getAttrOfType<ArrayAttr>("axes");
+  std::vector<int> axes;
+  int outRank = 0;
+  if (axisAttrs) {
+    outRank = inRank + axisAttrs.getValue().size();
+    for (auto axisAttr : axisAttrs.getValue()) {
+      int axis = axisAttr.cast<IntegerAttr>().getInt();
+      axis = axis >= 0 ? axis : (outRank + axis);
+      // Valid range
+      assert(axis >= -outRank && axis <= outRank - 1);
+      if (std::find(axes.begin(), axes.end(), axis) == axes.end())
+        axes.push_back(axis);
+      else
+        emitError("Duplicated axes.");
+    }
+  } else {
+    emitError("Axes attribute is required.");
+    return;
+  }
+
+  SmallVector<int64_t, 4> dims;
+  for (int i = 0, j = 0; i < outRank || j < inRank; ++i) {
+    if (std::find(axes.begin(), axes.end(), i) != axes.end()) {
+      dims.push_back(1);
+    } else {
+      dims.push_back(operandTy.getShape()[j++]);
+    }
+  }
+  getResult().setType(RankedTensorType::get(dims, operandTy.getElementType()));
+}
+
+//===----------------------------------------------------------------------===//
 // TableGen'd op method definitions
 //===----------------------------------------------------------------------===//
 
--- src/pass/shape_inference_pass.cpp
+++ src/pass/shape_inference_pass.cpp
@@ -118,7 +118,8 @@ public:
         op->getName().getStringRef() != "onnx.Reshape" &&
         op->getName().getStringRef() != "onnx.Transpose" &&
         op->getName().getStringRef() != "onnx.Softmax" &&
-        op->getName().getStringRef() != "onnx.ConvNoBias")
+        op->getName().getStringRef() != "onnx.ConvNoBias" &&
+        op->getName().getStringRef() != "onnx.Unsqueeze")
       return false;
     return llvm::any_of(op->getResultTypes(), [](Type result_type) {
       return !result_type.isa<RankedTensorType>();
